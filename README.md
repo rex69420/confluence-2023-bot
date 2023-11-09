7ef47cd3ed3b1f3d5d641b955b8febe3ce169318
rex69420 <65942753+rex69420@users.noreply.github.com>
2023-11-09 23:47:59 +0530

    init!

---
+++ README.md
@@ -0,0 +1,22 @@
+# Confluence 2023 Discord Bot
+
+This bot is designed to run using Python and the Discord.py library. This was created for Confluence 2023.
+
+## Installation Steps
+
+1. Clone the repository.
+2. Install the required dependencies using pip: `pip install -r requirements.txt`
+3. Rename the `.sample.env` file to `.env` and fill in the required fields.
+4. Run the bot by executing the Python script.
+```bash
+python bot.py
+```
+
+## Commands
+
+- Use the following commands in Discord after inviting the bot to a server:
+
+    - `/sendmessage <message or link>`: Sends a message to the current channel.
+    - `/editmessage <link> <message_id>`: Edits a message in the current channel.
+    - `/sendgif <link>`: Sends a GIF to the current channel.
+    - `$flag`: Sends the flag if you direct message the bot.
---
+++ bot.py
@@ -0,0 +1,145 @@
+import io
+import logging
+import os
+from datetime import datetime
+
+import aiohttp
+import discord
+import requests
+from discord.ext import commands
+from dotenv import load_dotenv
+from pytz import timezone
+
+logging.basicConfig(
+    filename="bot.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.Formatter.converter = lambda *args: datetime.now(
+    tz=timezone("Asia/Kolkata")
+).timetuple()
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logging.getLogger().addHandler(console_handler)
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+FLAG = os.getenv("FLAG")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$", intents=intents)
+client = discord.Client(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    logging.info(f"logged in as {bot.user.name} ({bot.user.id})")
+    await bot.tree.sync()
+    await bot.change_presence(activity=discord.Game(name="with life"))
+
+
+@bot.hybrid_command()
+async def sendmessage(ctx, link):
+    if isinstance(ctx.channel, discord.channel.TextChannel):
+        if ctx.author.guild_permissions.administrator:
+            channel = ctx.channel
+            try:
+                if link.startswith("http://") or link.startswith("https://"):
+                    response = requests.get(link)
+                    if response.status_code == 200:
+                        content = response.text
+                        await channel.send(content)
+                        await ctx.send("Message sent successfully.", ephemeral=True)
+                        logging.info(f"message sent by {ctx.author.name}")
+                    else:
+                        await ctx.send(
+                            "Failed to fetch the content from the provided link.",
+                            ephemeral=True,
+                        )
+                else:
+                    await channel.send(link)
+                    await ctx.send("Message sent successfully.", ephemeral=True)
+                    logging.info(f"message sent by {ctx.author.name}")
+            except Exception as e:
+                await ctx.send(f"An error occurred: {e}", ephemeral=True)
+                logging.warning(f"error occurred: {e}")
+        else:
+            await ctx.send(
+                "You don't have permissions to send the message.", ephemeral=True
+            )
+
+
+@bot.hybrid_command()
+async def editmessage(ctx, link, message_id):
+    if isinstance(ctx.channel, discord.channel.TextChannel):
+        if ctx.author.guild_permissions.administrator:
+            channel = ctx.channel
+            try:
+                if link.startswith("http://") or link.startswith("https://"):
+                    response = requests.get(link)
+                    if response.status_code == 200:
+                        content = response.text
+                        message = await channel.fetch_message(message_id)
+                        await message.edit(content=content)
+                        await ctx.send("Message edited successfully.", ephemeral=True)
+                        logging.info(
+                            f"message {message_id} edited by {ctx.author.name}"
+                        )
+                    else:
+                        await ctx.send(
+                            "Failed to fetch the content from the provided link.",
+                            ephemeral=True,
+                        )
+                else:
+                    message = await channel.fetch_message(message_id)
+                    await message.edit(content=link)
+                    await ctx.send("Message edited successfully.", ephemeral=True)
+                    logging.info(f"message {message_id} edited by {ctx.author.name}")
+            except Exception as e:
+                await ctx.send(f"An error occurred: {e}", ephemeral=True)
+                logging.warning(f"error occurred: {e}")
+        else:
+            await ctx.send(
+                "You don't have permissions to edit the message.", ephemeral=True
+            )
+
+
+@bot.hybrid_command()
+async def sendgif(ctx, link):
+    if isinstance(ctx.channel, discord.channel.TextChannel):
+        if ctx.author.guild_permissions.administrator:
+            channel = ctx.channel
+            try:
+                if link.startswith("http://") or link.startswith("https://"):
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(link) as resp:
+                            if resp.status == 200:
+                                data = io.BytesIO(await resp.read())
+                                await channel.send(file=discord.File(data, "attachment.gif"))
+                                await ctx.send("GIF sent successfully.", ephemeral=True)
+                                logging.info(f"gif sent by {ctx.author.name}")
+                            else:
+                                await ctx.send(
+                                    "Failed to fetch the content from the provided link.",
+                                    ephemeral=True,
+                                )
+                else:
+                    await ctx.send("Invalid link.", ephemeral=True)
+            except Exception as e:
+                await ctx.send(f"An error occurred: {e}", ephemeral=True)
+                logging.warning(f"error occurred: {e}")
+        else:
+            await ctx.send(
+                "You don't have permissions to send the attachment.", ephemeral=True
+            )
+
+
+@bot.command()
+async def flag(ctx):
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        logging.info(f"flag called by {ctx.author.name}")
+        await ctx.send(f"hi! here's your flag: `{FLAG}`", ephemeral=True)
+
+
+bot.run(TOKEN)
